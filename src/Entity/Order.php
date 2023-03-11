f9cd768199d0cbca364bda559e881da00c600f62
AnfFun <prokofmax@gmail.com>
2023-03-11 18:23:50 +0200

    add entity for our site

---
+++ src/Entity/Order.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrderRepository::class)]
+#[ORM\Table(name: '`order`')]
+class Order
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\OneToOne(inversedBy: 'orders', cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Customer $customer = null;
+
+    #[ORM\OneToMany(mappedBy: 'orders', targetEntity: Shoe::class)]
+    private Collection $shoes;
+
+    public function __construct()
+    {
+        $this->shoes = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCustomer(): ?Customer
+    {
+        return $this->customer;
+    }
+
+    public function setCustomer(Customer $customer): self
+    {
+        $this->customer = $customer;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Shoe>
+     */
+    public function getShoes(): Collection
+    {
+        return $this->shoes;
+    }
+
+    public function addShoe(Shoe $shoe): self
+    {
+        if (!$this->shoes->contains($shoe)) {
+            $this->shoes->add($shoe);
+            $shoe->setOrders($this);
+        }
+
+        return $this;
+    }
+
+    public function removeShoe(Shoe $shoe): self
+    {
+        if ($this->shoes->removeElement($shoe)) {
+            // set the owning side to null (unless already changed)
+            if ($shoe->getOrders() === $this) {
+                $shoe->setOrders(null);
+            }
+        }
+
+        return $this;
+    }
+}
